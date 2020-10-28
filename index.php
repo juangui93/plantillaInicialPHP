63c4696f0199d1d001dbcb71352300d608d39343
Juan Guillermo Del Valle Urrego <juangui93@gmail.com>
2020-10-27 23:05:43 -0500

    Se crea archivo index.php y se utiliza sólo etiquetas HTML

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+<title>Plantilla de Juan Guillermo Del Valle Urrego</title>
+</head>
+
+<body>
+
+	<header>
+		<div id="autor"></div>
+	</header>
+	
+	<h1>Bienvenido a mi sitio personal</h1>
+	
+	<section>
+		
+		<div id="caja1">
+			<p id="poema">"En el cielo dibujaré una estrella,
+				que será la luz de tu camino...
+				Siempre estaré contigo,
+				hasta el final de los días.
+				
+				Volaré tan alto, que podré mirar tu alma,
+				Expresarte cuánto te amo
+				Y ver más allá de tus ojos,
+				ojos que brillan como las estrellas del cielo." </p>
+		</div>
+		
+    	<div id="caja2">
+			<p id="descripción">
+				Soy amante de la música y la poesía. 
+				De soñar bonito y cantar con alegría. 
+				
+				De llorar cuando la soledad se apodera,
+				de reir cuando siento gozo. 
+				
+				De gozarme la vida y conocer lo bello, 
+				de vivir la vida, de instante en instante.
+			</p>
+		</div>
+		
+    	<div id="caja3"></div>
+		
+    	<div id="caja4">
+			
+			<img src="img/juan.jpg">
+			
+		</div>
+	</section>
+	
+	<footer id="footer">
+			<img src="img/footer.jpg""img/copyright.png" alt="Derechos de autor">
+		</footer>
+	
+	<div id="redes">
+		<ul>
+			<li id="facebook"><a href="https://www.facebook.com/juandelvalleurrego" target="_blank"><img src="img/facebook.png" alt="Facebook" class="btn"></a></li>
+			<li id="instagram"><a href="https://www.instagram.com/juandelvalleurrego/?hl=es-la" target="_blank"><img src="img/instagram.png" alt="Instagram" class="btn"></a></li>
+			<li id="youtube"><a href="https://www.youtube.com/channel/UClQU0C-bs_VlKe4uCMwLgkA?view_as=subscriber" target="_blank"><img src="img/youtube.png" alt="Youtube" class="btn"></a></li>
+			<li id="twitter"><a href="https://twitter.com/jdel_valle11" target="_blank"><img src="img/twitter.png" alt="Twitter" class="btn"></a></li>
+			
+		</ul>
+		
+		
+		
+	</div>
+</body>
+</html>
